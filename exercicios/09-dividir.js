00e5a791f1cd9bc9507292c861d024e00416b41a
CarlosQw32 <ncarlos63@gmail.com>
2023-06-07 21:35:19 -0300

    ajuste na variavel quantidade e descrição

--- exercicios/09-dividir.js
+++ exercicios/09-dividir.js
@@ -1,20 +1,21 @@
-function run(array, quantidade) {
-    const resultado = [];
-    const tamanhoSubarray = Math.ceil(array.length / quantidade);
+function dividir(array, quantidade) {
+  let resultado = [];
+  let tamanhoSubArray = Math.ceil(array.length / quantidade);
+
   
-    let indice = 0;
-    while (indice < array.length) {
-      const subarray = array.slice(indice, indice + tamanhoSubarray);
+  for (let i = 0; i < array.length; i += tamanhoSubArray) {
+      let subarray = array.slice(i, i + tamanhoSubArray);
       resultado.push(subarray);
-      indice += tamanhoSubarray;
     }
-  
-    return resultado;
-  }
-  
-  // Exemplo de uso
-  const array = [1, 2, 3, 4, 5];
-  const quantidade = 2;
-  const resultado = run(array, quantidade);
-  console.log(resultado);
-  
+
+  return resultado;
+}
+
+// Exemplo de uso
+const array = [1, 2, 3, 4, 5];
+const quantidade = 3;
+const resultado = dividir(array, quantidade);
+console.log(resultado);
+
+/**A função retorna um novo array com os subarrays criados a partir do array original, onde cada subarray tem um tamanho aproximadamente igual, exceto o último subarray que pode ter um tamanho menor.
+No exemplo de uso fornecido, o array [1, 2, 3, 4, 5] é dividido em 3 subarrays. O resultado é armazenado na variável "resultado" e é exibido no console. */
